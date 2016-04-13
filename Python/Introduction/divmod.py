9378c9623f299f358ea570cc0fb1da3d840c4cdb
Chris Gomes <ugali_guy@hotmail.com>
2016-04-12 23:57:56 -0500

    Submit 4 files for HackerRank Python Intro challenges

---
+++ Python/Introduction/divmod.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# __future__ allows Python 3 functionality in Python 2 code
+from __future__ import division 
+
+a = int(raw_input())
+b = int(raw_input())
+
+quotient = a//b
+remainder = a%b
+
+print quotient
+print remainder
+print divmod(a, b)
