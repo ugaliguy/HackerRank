ede7c7ea134a78ea10523d6bfc1a6711dd87081a
Chris Gomes <ugali_guy@hotmail.com>
2016-04-13 10:11:55 -0500

    Submit code for big integer challenge

---
+++ Python/Introduction/integers-come-in-all-sizes.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# __future__ allows Python 3 functionality in Python 2 code
+from __future__ import division 
+
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+big_int = (a**b) + (c**d)
+
+print big_int
