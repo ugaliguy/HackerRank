2a838d98faf242738f52a3950112dab2b110c7bc
Chris Gomes <ugali_guy@hotmail.com>
2016-04-13 10:31:53 -0500

    Submit code for interchange two numbers challenge

---
+++ Python/Introduction/interchange-two-numbers.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# __future__ allows Python 3 functionality in Python 2 code
+from __future__ import division 
+
+a = int(raw_input())
+b = int(raw_input())
+
+swap = (b,a)
+
+print swap[0]
+print swap[1]
