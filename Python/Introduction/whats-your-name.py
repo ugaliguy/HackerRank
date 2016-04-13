f6966d95acc13a63b7320d847e3f3e6d0ebb04d1
Chris Gomes <ugali_guy@hotmail.com>
2016-04-13 10:22:27 -0500

    Submit code for input/output challenge

---
+++ Python/Introduction/whats-your-name.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+first_name = raw_input()
+last_name = raw_input()
+
+print "Hello " + str(first_name) + " " + str(last_name) + "! You just delved into python."
