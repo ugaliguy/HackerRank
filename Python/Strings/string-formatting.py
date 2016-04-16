1335ca28cce988c5764b15615cc90e9b8e43640c
Chris Gomes <ugali_guy@hotmail.com>
2016-04-15 22:34:21 -0500

    Submit code for string-formatting challenge

---
+++ Python/Strings/string-formatting.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(raw_input())
+
+width = len("{0:b}".format(n)) 
+
+for i in range(1, n + 1):
+	print "{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width  = width)
