ad8e74bdc42119a12260ef5c4067d7603ee8ac05
Chris Gomes <ugali_guy@hotmail.com>
2016-04-18 18:51:24 -0500

    Submit code for captains room

--- Python/Sets/captains-room.py
+++ Python/Sets/captains-room.py
@@ -1,1 +1,8 @@
-captains-room.py
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+size = int(raw_input()) #The size of each group
+guests = list(map(int, raw_input().split()))
+rooms = set(guests) # The set of distinct room numbers
+
+room = (sum(rooms)*size - sum(guests))/(size - 1)
+
+print room
