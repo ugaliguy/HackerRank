7faf81df9da75cab426c00fd76795a90ab7d8d7a
Chris Gomes <ugali_guy@hotmail.com>
2016-05-10 20:59:32 -0500

    Fix typo in product

--- Python/Numpy/np-sum-and-prod.py
+++ Python/Numpy/np-sum-and-prod.py
@@ -4,11 +4,12 @@ n, m = map(int, raw_input().split())
 
 a = []
 for i in range(n):
-    a.append(map(int, raw_input().split()))
+    a.append(list(map(int, raw_input().split())))
     
 a = numpy.array(a)
+a = numpy.reshape(a, (n,m))
 
 a_sum = numpy.sum(a, axis = 0)
-result = numpy.product(a)
+result = numpy.product(a_sum)
 
 print result
