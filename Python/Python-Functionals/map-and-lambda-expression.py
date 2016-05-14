d0889d9efb0a3983b4e23c0c91762810fa165766
Chris Gomes <ugali_guy@hotmail.com>
2016-05-14 17:14:26 -0500

    Submit code for Fibonacci cubed

--- Python/Python-Functionals/map-and-lambda-expression.py
+++ Python/Python-Functionals/map-and-lambda-expression.py
@@ -1,1 +1,12 @@
-map-and-lambda-expression.py
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(raw_input())
+
+fib = lambda y: y if y < 2 else fib(y - 1) + fib(y - 2) # Calculates nth Fibonacci number from 0th Fibonacci number
+fibs = lambda n: map(fib, range(n))
+
+cube = lambda a: a**3
+
+result = map(cube, fibs(n))
+
+print result
