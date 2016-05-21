6c75ea10578cbfc2a485198c07eb9b75395917dd
Chris Gomes <ugali_guy@hotmail.com>
2016-05-21 11:58:57 -0500

    Update files to include instructions before the code

--- Java/Introduction/java-if-else.java
+++ Java/Introduction/java-if-else.java
@@ -1,3 +1,12 @@
+// This problem will test your knowledge on "if-else" statements.
+
+// Given an integer  as input, check the following:
+
+// If  is odd, print "Weird".
+// If  is even and, in between the range of 2 and 5(inclusive), print "Not Weird".
+// If  is even and, in between the range of 6 and 20(inclusive), print "Weird".
+// If  is even and , print "Not Weird". 
+
     import java.io.*;
     import java.util.*;
     import java.text.*;
--- Java/Introduction/welcome-to-java.java
+++ Java/Introduction/welcome-to-java.java
@@ -1,1 +1,14 @@
-welcome-to-java.java
+// Welcome to the world of Java! 
+// Just print "Hello World." and "Hello Java." in two separate lines 
+// to complete this challenge.
+
+public class Solution {
+   
+   public static void main(String []argv)
+   {
+      //Write your solution here.
+       System.out.println("Hello World.");
+           System.out.println("Hello Java.");
+   }
+
+}
