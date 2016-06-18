d4948d11c88d47f21934ed86887fd8251728de1f
Chris Gomes <ugali_guy@hotmail.com>
2016-06-17 20:38:43 -0500

    Submit code for factorials

---
+++ Algorithms/Implementation/extra-long-factorials.py
@@ -0,0 +1,12 @@
+# Python 2
+
+# Note this is not complicated in Python since
+# Python automatically handles big integers
+
+import sys
+import math
+
+n = int(raw_input().strip())
+answer = math.factorial(n)
+
+print answer
