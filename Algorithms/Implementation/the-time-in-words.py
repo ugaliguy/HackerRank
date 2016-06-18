e46b103d4eb71f1abffd8c3f3abba95556bc6b15
Chris Gomes <ugali_guy@hotmail.com>
2016-06-18 08:21:52 -0500

    Submit code using dictionary

---
+++ Algorithms/Implementation/the-time-in-words.py
@@ -0,0 +1,27 @@
+# Python 2
+
+import sys
+
+
+h = int(raw_input())
+m = int(raw_input())
+
+numb = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven',
+        8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen',
+        14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
+        19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two',
+        23: 'twenty three', 24: 'twenty four', 25: 'twenty five', 26: 'twenty seven',
+        28: 'twenty eight', 29: 'twenty nine'}
+    
+if m == 0:
+    print numb[h] +' o\' clock'
+elif m == 15:
+    print 'quarter past '+ numb[h]
+elif m < 30:
+    print numb[m] + ' minutes past ' + numb[h]
+elif m == 30:
+    print 'half past '+ numb[h]
+elif m == 45:
+    print 'quarter to '+ numb[(h+1)%12]
+else:
+    print numb[60-m] + ' minutes to ' + numb[(h+1)%12]
