06a972e1f261a9b96dfc9f056e01e8d10776487f
Chris Gomes <ugali_guy@hotmail.com>
2016-06-21 16:57:40 -0500

    Add problem description

--- Algorithms/Warmup/simple-array-sum.py
+++ Algorithms/Warmup/simple-array-sum.py
@@ -1,3 +1,6 @@
+# You are given an array of integers of size . You need to print the sum of 
+# the elements in the array, keeping in mind that some of those integers may be quite large.
+
 import sys
 
 
