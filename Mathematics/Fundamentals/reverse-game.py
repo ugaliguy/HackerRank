c8de3000c667f1c9d97d44e6247e6423981542d3
Chris Gomes <ugali_guy@hotmail.com>
2016-06-21 21:22:19 -0500

    Submit codes for pattern checking and telescoping series

--- Mathematics/Fundamentals/reverse-game.py
+++ Mathematics/Fundamentals/reverse-game.py
@@ -1,1 +1,11 @@
-reverse-game.py
+# Python 2
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+t = int(raw_input())
+for i in range(t):
+    n,k = [int(x) for x in raw_input().split()]
+    if(k>=n/2):
+        print(2*(n-k-1))
+    else:
+        print(2*k+1)
