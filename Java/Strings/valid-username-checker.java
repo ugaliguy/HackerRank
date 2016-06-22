55e413c82be7aa51265c991e8a54912a3220bab7
Chris Gomes <ugali_guy@hotmail.com>
2016-06-21 21:19:22 -0500

    Submit code for username regex

--- Java/Strings/valid-username-checker.java
+++ Java/Strings/valid-username-checker.java
@@ -4,13 +4,13 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
 public class Solution {
    public static void main(String[] args){
       Scanner in = new Scanner(System.in);
       int testCases = Integer.parseInt(in.nextLine());
       while(testCases>0){
          String username = in.nextLine();
-
          String pattern = "^[a-zA-Z][A-Za-z0-9_]{7,29}$";
          Pattern r = Pattern.compile(pattern);
          Matcher m = r.matcher(username);
