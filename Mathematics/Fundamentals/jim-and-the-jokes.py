dc56d2634f05ec57ba594c5c70193d2b113c53e5
Chris Gomes <ugali_guy@hotmail.com>
2016-06-22 15:40:50 -0500

    Fix code to deal with large numbers

--- Mathematics/Fundamentals/jim-and-the-jokes.py
+++ Mathematics/Fundamentals/jim-and-the-jokes.py
@@ -15,13 +15,21 @@ def base(m,d): # Converts d (base m) to its decimal equivalent
     return result
 
 n = int(raw_input())
-decimals = [0]*37 # 37 = 12*3 + 1 = max(base(i,j)) for 1<=i<=12 (months) and 1<=j<=31 (days)
+decimals = [0]*38 # 37 = 12*3 + 1 = max(base(i,j)) for 1<=i<=12 (months) and 1<=j<=31 (days)
 for i in range(n):
     m,d = [int(j) for j in raw_input().split()]
     x = base(m,d)
-    result = 0
+    #print 'x = ' + str(x)
     if(x != -1):
-        result += decimals[x]
         decimals[x] += 1
+
+answer = 0
+for j in decimals:
+    # Note that the following if-else  is necessary in order
+    # to deal with large integers
+    if j%2 == 0:
+        answer += (j/2)*(j-1)
+    else:
+        answer += ((j-1)/2)*j
         
-print result
+print answer 
