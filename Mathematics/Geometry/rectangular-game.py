52b9588d154181972d2e8cb2fd93ebd0046ed927
Chris Gomes <ugali_guy@hotmail.com>
2016-06-27 15:58:39 -0500

    Submit code using min function

---
+++ Mathematics/Geometry/rectangular-game.py
@@ -0,0 +1,19 @@
+rectangular-game.py
+
+# Python 2
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(raw_input())
+a_coord = []
+b_coord = []
+for i in range(n):
+    a,b = [int(j) for j in raw_input().split()]
+    a_coord.append(a)
+    b_coord.append(b)
+    
+min_a = min(a_coord)
+min_b = min(b_coord)
+
+answer = min_a*min_b
+print answer
