5c4807d53082f6be25432ad00009f2ae555dab2f
Chris Gomes <ugali_guy@hotmail.com>
2016-08-23 22:31:46 -0500

    Rewrite code for efficiency

--- Algorithms/Implementation/non-divisible-subset.py
+++ Algorithms/Implementation/non-divisible-subset.py
@@ -4,26 +4,39 @@ n,k = [int(i) for i in raw_input().split()]
 
 numbers = [int(i) for i in raw_input().split()]
 
+counts = [0] * k
+for number in numbers:
+    counts[number % k] += 1
+
+count = min(counts[0], 1)
+for i in range(1, k//2+1):
+    if i != k - i:
+        count += max(counts[i], counts[k-i])
+if k % 2 == 0: 
+    count += 1
+
+print count
+
 
 # This code has a bug in it - Fix it!
 
-mod_k = {j:[] for j in range(k)}
+# mod_k = {j:[] for j in range(k)}
 
-for i in range(n):
-    mod_k[numbers[i]%k].append(numbers[i])
+# for i in range(n):
+#     mod_k[numbers[i]%k].append(numbers[i])
 
-count = 0
-if len(mod_k[0]) > 1:
-    count = 1
+# count = 0
+# if len(mod_k[0]) > 1:
+#     count = 1
     
-pairs = set([(j,k-j) for j in range(1,k/2+1)])  
-for p,q in pairs:
-    if p != q:
-        if len(mod_k[p]) > len(mod_k[q]):
-            count += len(mod_k[p])
-        else:
-            count += len(mod_k[q])
-    else:
-        if len(mod_k[p]) > 0:
-            count += 1
-print count
+# pairs = set([(j,k-j) for j in range(1,k/2+1)])  
+# for p,q in pairs:
+#     if p != q:
+#         if len(mod_k[p]) > len(mod_k[q]):
+#             count += len(mod_k[p])
+#         else:
+#             count += len(mod_k[q])
+#     else:
+#         if len(mod_k[p]) > 0:
+#             count += 1
+# print count
