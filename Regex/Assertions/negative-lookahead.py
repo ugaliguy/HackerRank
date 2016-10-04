f4301e98be0f185ce7bf5e974457703bf02e7980
Chris Gomes <ugali_guy@hotmail.com>
2016-10-04 10:36:32 -0500

    Submit regex for a negative lookahead

---
+++ Regex/Assertions/negative-lookahead.py
@@ -0,0 +1,10 @@
+import re
+
+
+Test_String = raw_input()
+
+
+Regex_Pattern = r"(.)(?!\1)"	# Do not delete 'r'.
+match = re.findall(Regex_Pattern, Test_String)
+
+print "Number of matches :", len(match)
